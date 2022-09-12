e5906a1ecabc2f7b8c53a9004209c929becc62c5
andrecodelima <a.andreluislima@gmail.com>
2022-09-12 18:03:52 -0300

    criacao de responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,19 @@ function abrir(){
     alert('SEJA BEM-VINDO AO MEU PORTIFÓLIO! \n' + 
            'ATENÇÃO SITE EM CONSTRUÇÃO!')
 }
+
+
+const menu = document.getElementById("menu-list")
+const btnToggle = document.getElementById("menu-toggle")
+const pagina = document.getElementById("section-sobre")
+
+function exibirMenu(){
+    btnToggle.style.display = 'none'
+    menu.style.display = 'block'
+}
+
+function exibirMenus(){
+    pagina.style.display = 'none'
+    menu.style.display = 'block'
+
+}
