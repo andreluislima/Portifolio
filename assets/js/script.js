b44f9650098ce310370de870d89b8b428d4a9f87
andrecodelima <a.andreluislima@gmail.com>
2022-09-14 18:04:03 -0300

    responsive edicoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ function abrir(){
 
 const menu = document.getElementById("menu-list")
 const btnToggle = document.getElementById("menu-toggle")
-const pagina = document.getElementById("section-sobre")
+// const pagina = document.getElementById("section-sobre")
 
 // HOME
 function exibirMenuHome(){
@@ -17,7 +17,24 @@ function exibirMenuHome(){
 }
 
 function exibirMenus(){
-    pagina.style.display = 'none'
+
+    let home;
+    let sobre;
+    let resume;
+    let portifolio;
+    let contatos;
+
     menu.style.display = 'block'
 
+    sobre = document.getElementById("section-sobre")
+    sobre.style.display = 'none'
+
+    resume = document.getElementById("section-resume")
+    resume.style.display = 'none'
+
+    portifolio = document.getElementById("section-portifolio")
+    portifolio.style.display = 'none'
+
+    contatos = document.getElementById("section-contatos")
+    contatos.style.display = 'none'
 }
